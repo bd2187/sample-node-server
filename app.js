08dfdd9c1a1fbc4833777755a6cd29c34cb60024
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-11 21:02:47 -0700

    Set up default middleware, routes, and port

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+// middleware / static files
+app.use(express.static(`${__dirname}/public`));
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+// port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Now listening to port ${port}`);
+});

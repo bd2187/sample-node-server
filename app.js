007aee1332fb059234821299c58065a9338b128e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-11 22:13:38 -0700

    Create head and nav partials. Inject partials into home, contact and about page

--- app.js
+++ app.js
@@ -5,10 +5,25 @@ const app = express();
 // middleware / static files
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
+hbs.registerPartials(`${__dirname}/views/partials`);
 
 // routes
 app.get('/', (req, res) => {
-  res.render('home.hbs');
+  res.render('home', {
+    title: 'Home'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    title: 'Contact'
+  });
 });
 
 // port

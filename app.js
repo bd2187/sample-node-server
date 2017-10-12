a029c46cccf1a425c62253ffde3bc1518d9c6f57
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-11 22:59:38 -0700

    Add middleware to append visitLog.txt with currentTime

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
+// built-in modules
+const fs = require('fs');
+
+// 3rd party modules
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+
+// local modules
 const routes = require('./controllers/routes');
 
 // middleware / static files
 app.set('view engine', 'hbs');
 app.use(express.static(`${__dirname}/public`));
+app.use(function(req, res, next) {
+  const currentTime = `${new Date()} \n`;
+  fs.appendFile('visitLog.txt', currentTime, err => {
+    if (err) throw err;
+    console.log('Someone visited your site');
+  });
+  next();
+});
 
 // register partials and helpers
 hbs.registerPartials(`${__dirname}/views/partials`);
